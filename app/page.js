469e3258fc38cd73c6e81c40828f115ef9f39f78
unifiednun <bitnunblockchain@gmail.com>
2025-08-20 11:18:07 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>UnifiedNUN 3.1</h1>
+      <p>Welcome to your browser-native blockchain.</p>
+    </main>
+  );
+}
