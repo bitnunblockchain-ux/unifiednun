216e0bac4b0cce54ac27b398776ba9234321a6e9
unifiednun <bitnunblockchain@gmail.com>
2025-08-20 16:03:59 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
 export const metadata = {
   title: 'UnifiedNUN',
-  description: 'Browser-native blockchain platform'
+  description: 'The browser-native blockchain for dreamers',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-black text-white font-sans">{children}</body>
     </html>
   );
 }
+
